b8c2d85bb23446f57194e847270af46d511f192e
PyaeSonePaing-104 <peterpaingcoding@gmail.com>
2026-07-02 19:35:46 +0630

    styled about section

--- my-app/Component/Main.jsx
+++ my-app/Component/Main.jsx
@@ -18,9 +18,7 @@ function Content(){
             <section id="main-about">
                 <h2>About</h2>
                 <p>I am a frontend developer with a particular interest in making things simple and automating daily tasks. I try to keep up with security and best practices, and am always looking for new things to learn.</p>
-            </section>
-            <section id="main-interests">
-                <h3>Interests</h3>
+                <h2>Interests</h2>
                 <p>Food expert. Music scholar. Reader. Internet fanatic. Bacon buff. Entrepreneur. Travel geek. Pop culture ninja. Coffee fanatic.</p>
             </section>
             </>
